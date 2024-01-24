409deea4878c865d203e0ce9e30b82c3b64b31e1
Tatsiana <tatsianachubryk@gmail.com>
2024-01-24 14:30:58 +0300

    add data

--- app/src/main/java/com/example/diary/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/diary/fragments/add/AddFragment.kt
@@ -1,13 +1,22 @@
 package com.example.diary.fragments.add
 
+import android.icu.text.CaseMap.Title
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.diary.R
+import com.example.diary.data.models.DiaryEntity
+import com.example.diary.data.models.Priority
+import com.example.diary.data.viewmodel.DiaryViewModel
 import com.example.diary.databinding.FragmentAddBinding
 import com.example.diary.databinding.FragmentListBinding
 import java.text.SimpleDateFormat
@@ -17,6 +26,7 @@ import java.util.logging.SimpleFormatter
 class AddFragment : Fragment() {
 
     private lateinit var binding: FragmentAddBinding
+    private val mDiaryViewModel: DiaryViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,6 +46,48 @@ class AddFragment : Fragment() {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.add_fragment_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_add){
+            insertDataToDB()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun insertDataToDB() {
+        val mDate = binding.dateEt.text.toString()
+        val mPriority = binding.prioritiesSpinner.selectedItem.toString()
+        val mDescription = binding.descriptionEt.text.toString()
+
+        val validation = verifyDataFromUser(mDate, mDescription)
+        if(validation){
+            val newData = DiaryEntity(
+                0,
+                mDate,
+                parsePriority(mPriority),
+                mDescription
+            )
+            mDiaryViewModel.insertData(newData)
+            Toast.makeText(requireContext(), "Successful", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        }
+    }
+
+    private fun verifyDataFromUser(date: String, description: String): Boolean {
+        return if(TextUtils.isEmpty(date) || TextUtils.isEmpty(description)){
+            false
+        } else !(date.isEmpty() || description.isEmpty())
+    }
+
+    private fun parsePriority(priority: String): Priority{
+        return when(priority){
+            "High Priority" -> {Priority.HIGH}
+            "Medium Priority" -> {Priority.MEDIUM}
+            "Low Priority" -> {Priority.LOW}
+            else -> Priority.LOW
+        }
+
     }
 
     private fun dateFormat() {
