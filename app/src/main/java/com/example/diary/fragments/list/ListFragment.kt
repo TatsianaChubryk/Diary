ecdf8641534a500c736052fe681eb8ccf9d6f797
Tatsiana <tatsianachubryk@gmail.com>
2024-01-26 15:23:00 +0300

    swipeToDelete

--- app/src/main/java/com/example/diary/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/diary/fragments/list/ListFragment.kt
@@ -7,10 +7,13 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.diary.R
 import com.example.diary.data.viewmodel.DiaryViewModel
 import com.example.diary.databinding.FragmentListBinding
@@ -31,6 +34,8 @@ class ListFragment : Fragment() {
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(requireActivity())
 
+        swipeToDelete(recyclerView)
+
         mDiaryViewModel.getAllData.observe(viewLifecycleOwner) { data ->
             adapter.setData(data)
         }
@@ -42,6 +47,19 @@ class ListFragment : Fragment() {
         setHasOptionsMenu(true)
 
         return binding.root
+    }
+
+    private fun swipeToDelete(recyclerView: RecyclerView) {
+        val swipeToDeleteCallback = object : SwipeToDelete() {
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val itemToDelete = adapter.dataList[viewHolder.adapterPosition]
+                // Delete Item
+                mDiaryViewModel.deleteItem(itemToDelete)
+                Toast.makeText(requireContext(), "Successful", Toast.LENGTH_SHORT).show()
+            }
+        }
+        val itemTouchHelper = ItemTouchHelper(swipeToDeleteCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
